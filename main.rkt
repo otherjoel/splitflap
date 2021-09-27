fd3b5fadfe5bdd00b6bfe0333c08dcc3b2b6010b
Joel Dueck <joel@jdueck.net>
2021-09-26 22:34:37 -0500

    Build Atom and RSS 2.0 feeds

--- main.rkt
+++ main.rkt
@@ -2,39 +2,102 @@
 
 (require "constructs.rkt"
          gregor
-         pollen/template
          txexpr
-         racket/contract)
+         racket/contract
+         racket/match
+         racket/port
+         xml)
 
-(define (as-cdata string)
-  (format "<![CDATA[~a]]>" string))
+(define (as-cdata v)
+  (cdata #f #f (format "<![CDATA[~a]]>"
+                       (cond [(txexpr? v) (xexpr->string v)]
+                             [else v]))))
 
-(struct feed-entry (id uri title author published updated content)
-  #:guard (struct-guard/c tag-specific-string?
-                          valid-url-string?
-                          string?
-                          person?
-                          moment?
-                          moment?
-                          xexpr?))
+(struct feed-entry
+  (id url title author published updated content)
+  #:guard (struct-guard/c
+           tag-uri?
+           valid-url-string?
+           string?
+           person?
+           moment?
+           moment?
+           xexpr?))
 
-(define/contract (entry->xexpr e feed-uri dialect)
-  (-> feed-entry? tag-uri? rss-dialect? txexpr?)
+(define (entry-newer? maybe-newer other)
+  (moment>? (feed-entry-updated maybe-newer) (feed-entry-updated other)))
+
+(define/contract (entry-output e dialect #:result-type [result-type 'xml-string])
+  (->* (feed-entry? rss-dialect?) (#:result-type (or/c 'xml-string 'xml 'xexpr 'xexpr-cdata)) any/c)
+  (match-define (feed-entry id url title author published updated content) e)
+  (define xml? (memq result-type '(xml xml-string xexpr-cdata)))
+  (define entry-xpr
   (case dialect
-    [(rss)
-     `(item
-       (title ,(feed-entry-title e))
-       ,(person->xexpr (feed-entry-author e) 'author 'rss)
-       (link ,(feed-entry-uri e))
-       (guid ,(string-append (tag-uri->string feed-uri) "." (feed-entry-id e)))
-       (pubDate ,(moment->string (feed-entry-published e) 'rss))
-       (description ,(as-cdata (feed-entry-content e))))]
     [(atom)
      `(entry
-       (title ,(feed-entry-title e))
-       (id ,(string-append (tag-uri->string feed-uri) "." (feed-entry-id e)))
-       (link [[rel "alternate"] [href ,(feed-entry-uri e)]])
-       ,(person->xexpr (feed-entry-author e) 'author 'atom)
-       (updated ,(moment->string (feed-entry-updated e) 'atom))
-       (published ,(moment->string (feed-entry-published e) 'atom))       
-       (content ,(as-cdata (feed-entry-content e))))]))
+       (title ,title)
+       (id ,(tag-uri->string id))
+       (link [[rel "alternate"] [href ,url]])
+       ,(person->xexpr author 'author 'atom)
+       (updated ,(moment->string updated 'atom))
+       (published ,(moment->string published 'atom))  
+       (content ,(if xml? (as-cdata content) (cdata-string (as-cdata content)))))]
+    [(rss)
+     `(item
+       (title ,title)
+       ,(person->xexpr author 'author 'rss)
+       (link ,(tag-uri->string id))
+       (guid [[isPermaLink "false"]] ,(tag-uri->string id))
+       (pubDate ,(moment->string updated 'rss))
+       (description ,(if xml? (as-cdata content) (cdata-string (as-cdata content)))))]))
+  (case result-type
+    [(xexpr xexpr-cdata) entry-xpr]
+    [(xml) (xexpr->xml entry-xpr)]
+    [(xml-string) (display-xml-to-string (xexpr->xml entry-xpr))]))
+
+(struct feed
+  (id site-url name entries)
+  #:guard (struct-guard/c tag-uri? valid-url-string? xexpr? (listof feed-entry?)))
+
+(define (display-xml-to-string x)
+  (with-output-to-string
+    (λ ()
+      (parameterize ([empty-tag-shorthand (cons 'atom:link html-empty-tags)])
+        (display-xml/content x #:indentation 'peek)))))
+
+(define/contract (feed-output f feed-url dialect #:result-type [result-type 'xml-string])
+  (->* (feed? valid-url-string? rss-dialect?) (#:result-type (or/c 'xml-string 'xml 'xexpr)) (or/c string? content/c txexpr?))
+  (define entries-sorted (sort (feed-entries f) entry-newer?))
+  (match-define (feed feed-id site-url feed-name _) f)
+  (define gen (format "Racket v~a [~a] (https://racket-lang.org)" (version) (system-type 'gc)))
+  (define last-updated (feed-entry-updated (car entries-sorted)))
+  (define xml? (memq result-type '(xml xml-string)))
+  
+  (define feed-xpr
+    (case dialect
+      [(atom)
+       `(feed [[xmlns "http://www.w3.org/2005/Atom"]]
+              (title ,feed-name)
+              (generator ,gen)
+              (id ,(tag-uri->string feed-id))
+              (link [[rel "self"] [href ,feed-url]])
+              (link [[rel "alternate"] [href ,site-url]])
+              (updated ,(moment->string last-updated 'atom))
+              ,@(for/list ([e (in-list entries-sorted)])
+                  (entry-output e 'atom #:result-type (if xml? 'xexpr-cdata 'xexpr))))]
+      [(rss)
+       `(rss [[version "2.0"] [xmlns:atom "http://www.w3.org/2005/Atom"]]
+             (channel
+              (title ,feed-name)
+              (generator ,gen)
+              (description ,feed-name)
+              (atom:link [[rel "self"] [href ,feed-url] [type "application/rss+xml"]])
+              (link ,site-url)
+              (pubDate ,(moment->string last-updated 'rss))
+              (lastBuildDate ,(moment->string last-updated 'rss))
+              ,@(for/list ([e (in-list entries-sorted)])
+                  (entry-output e 'atom #:result-type (if xml? 'xexpr-cdata 'xexpr)))))]))
+  (case result-type
+    [(xexpr) feed-xpr]
+    [(xml) (xexpr->xml feed-xpr)]
+    [(xml-string) (display-xml-to-string (xexpr->xml feed-xpr))]))
